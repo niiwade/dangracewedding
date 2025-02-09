e2aadb8a77f52c132c28401cbf8dd6443a9441cf
Joseph Lamptey <josephlamptey@Josephs-MacBook-Pro.local>
2025-02-09 00:47:15 +0000

    third  commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,32 +1,30 @@
-import type { Metadata } from 'next';
+import type { Metadata } from "next";
 
-import './globals.css'
-import Navbar from '@/components/Navbar';
-import Footer from '@/components/Footer';
-import PrelineScript from '@/components/PrelineScript';
-import { Analytics } from "@vercel/analytics/react"
+import "./globals.css";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import PrelineScript from "@/components/PrelineScript";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata: Metadata = {
-  title: '#ALamp4Trey2024',
-  description: 'Official Wedding Website',
-}
+  title: "#DANNEL2025",
+  description: "Official Wedding Website",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body>
-        <Analytics/>
+        <Analytics />
         <Navbar />
-        <main className="relative overflow-hidden">
-          {children}
-        </main>
+        <main className="relative overflow-hidden">{children}</main>
         <Footer />
       </body>
-      <PrelineScript/>
+      <PrelineScript />
     </html>
-  )
+  );
 }
