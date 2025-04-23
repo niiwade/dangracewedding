fb62eaae915a70ac7f140d7b915f070d78fe27d2
Joseph Nii Lante Lamptey <niilantewade77@gmail.com>
2025-04-23 18:05:02 +0000

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -94,10 +94,10 @@ const Footer = () => {
 						Programme Outline
 					</a>
 				</nav>
-				<p className="py-10 text-center text-black">
+				<p className="py-5 text-center text-black">
 					2025 #Dannel2025 | All rights reserved
 				</p>
-				<p className="py-3 text-center ">
+				<p className="py-2 text-center ">
 					Powered by <span> <a href="https://www.revasint.com/" className="font-medium text-blue-500">
 						Revas
 					</a> </span>
